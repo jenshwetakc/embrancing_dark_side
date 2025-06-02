3cfb9555eddc809c08aaa6721a0589489ec6a45a
Shweta K.C <devshwetakc@gmail.com>
2025-06-02 11:33:13 +0900

    claude initial commit

---
+++ chromaeye/llm_model/claude_api/claude_api.py
@@ -0,0 +1,11 @@
+'''
+claude api model: claude-opus-4-20250514
+
+Compare the screenshots pairs of web-application pair in light and dark mode
+aesthetic to detect the inconsistency.
+
+Inconsistency such as invisible text, icon in dark mode.
+
+'''
+
+
