3d0325e21011a3be69bae7374257dbd2fe55432c
Shweta K.C <devshwetakc@gmail.com>
2025-06-02 11:47:55 +0900

    grok initial commit

---
+++ chromaeye/llm_model/grok_api/grok_api.py
@@ -0,0 +1,9 @@
+'''
+gork api model: "grok-2-vision-latest"
+
+Compare the screenshots pairs of web-application pair in light and dark mode
+aesthetic to detect the inconsistency.
+
+Inconsistency such as invisible text, icon in dark mode.
+
+'''
