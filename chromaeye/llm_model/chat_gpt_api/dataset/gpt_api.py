ec0216ae2d3a2ce064c0c5d206b7bd006d2f244a
Shweta K.C <devshwetakc@gmail.com>
2025-06-02 11:27:14 +0900

    chat gpt initial commit

---
+++ chromaeye/llm_model/chat_gpt_api/dataset/gpt_api.py
@@ -0,0 +1,9 @@
+'''
+Gpt api model: "gpt-4o"
+
+Compare the screenshots pairs of web-application pair in light and dark mode
+aesthetic to detect the inconsistency.
+
+Inconsistency such as invisible text, icon in dark mode.
+
+'''
