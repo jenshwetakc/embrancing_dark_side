b035d38b92ec22c41cedf317881492e86090431e
Shweta K.C <devshwetakc@gmail.com>
2025-06-02 11:39:09 +0900

    gemini initial commit

---
+++ chromaeye/llm_model/gemini_api/gemini_api.py
@@ -0,0 +1,9 @@
+'''
+gemini api model: gemini-2.5-pro-preview-05-06
+
+Compare the screenshots pairs of web-application pair in light and dark mode
+aesthetic to detect the inconsistency.
+
+Inconsistency such as invisible text, icon in dark mode.
+
+'''
